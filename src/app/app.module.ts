8043c988d3b4daad620bab6d729b45b4fbeb619b
rishabhv471 <rishabhv471@gmail.com>
2024-03-31 22:37:36 +0530

    first frontend commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoAngularMaterailModule } from './DemoAngularMaterailModule';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserModule,
+    BrowserModule,
+    AppRoutingModule,
+    DemoAngularMaterailModule
   ],
   providers: [],
   bootstrap: [AppComponent]
